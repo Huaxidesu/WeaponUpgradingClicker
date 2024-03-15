7b85a13a87ac0c9a4bef477a64b847168f29467e
Huaxidesu <Huaxidesu@gmail.com>
2024-03-15 08:42:41 +0800

    加点功能

---
+++ moreclick.py
@@ -0,0 +1,43 @@
+import pyautogui
+import time
+import keyboard
+
+clicking = False  # 点击状态
+interval = 0.5  # 默认点击间隔时间
+
+
+def set_interval(short):
+    global interval
+    interval = short
+    print(f"设置点击间隔时间为 {interval} 秒")
+
+
+def stop_clicking():
+    global clicking
+    clicking = False
+    print("停止点击")
+
+
+def end_program():
+    global running
+    running = False
+    print("结束程序")
+
+
+keyboard.add_hotkey("j", set_interval, args=(0.5,))  # 自己改键和时间
+keyboard.add_hotkey("k", set_interval, args=(1.0,))  # 自己改键和时间
+keyboard.add_hotkey("l", set_interval, args=(1.5,))  # 自己改键和时间
+keyboard.add_hotkey("right", stop_clicking)  # 当按下→键时，停止点击
+keyboard.add_hotkey("down", end_program)  # 当按下↓键时，结束程序
+
+running = True
+clicking = True
+try:
+    while running:
+        if clicking:
+            pyautogui.click()
+            time.sleep(interval)  # 等待点击间隔
+except KeyboardInterrupt:
+    print("程序被用户中断")
+
+print("程序结束")
