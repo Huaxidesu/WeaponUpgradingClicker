394a4691dbeca9059023489398182859ce730370
Huaxidesu <Huaxidesu@gmail.com>
2024-03-15 09:37:07 +0800

    1

--- moreclick.py
+++ moreclick.py
@@ -2,14 +2,14 @@ import pyautogui
 import time
 import keyboard
 
-clicking = False  # 点击状态
+clicking = False
 interval = 0.5  # 默认点击间隔时间
 
 
 def set_interval(short):
-    global interval
+    global interval, clicking
     interval = short
-    print(f"设置点击间隔时间为 {interval} 秒")
+    clicking = True
 
 
 def stop_clicking():
@@ -24,14 +24,13 @@ def end_program():
     print("结束程序")
 
 
-keyboard.add_hotkey("left", set_interval, args=(0.5,))  # 自己改键和时间
-keyboard.add_hotkey("up", set_interval, args=(1.0,))  # 自己改键和时间
-keyboard.add_hotkey("[", set_interval, args=(1.5,))  # 自己改键和时间
-keyboard.add_hotkey("right", stop_clicking)  # 当按下→键时，停止点击
-keyboard.add_hotkey("down", end_program)  # 当按下↓键时，结束程序
+keyboard.add_hotkey("left", set_interval, args=(0.5,))  # 自己改键
+keyboard.add_hotkey("up", set_interval, args=(1.0,))    # 自己改键
+keyboard.add_hotkey("[", set_interval, args=(1.5,))    # 自己改键
+keyboard.add_hotkey("right", stop_clicking)            # 停止点击
+keyboard.add_hotkey("down", end_program)               # 结束程序
 
 running = True
-clicking = False
 try:
     while running:
         if clicking:
